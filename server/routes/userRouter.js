d358e71d68e375ab962c13615beea44ea19fa5a7
mikhail <misha7877@gmail.com>
2022-01-08 16:48:49 +0500

    added check role and auth middleware, added check role to routers

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/AuthMiddleware')
 
-router.post('/register', userController.register)
+router.post('/register', authMiddleware, userController.register)
 router.post('/login', userController.login)
 router.get('/auth', userController.auth)
 
